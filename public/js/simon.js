caa148ad4edfb0cc643fa07893f09667d05eec14
Gaston Lenotre <gastonlenotre@gmail.com>
2016-02-22 13:44:06 -0600

    css js folders

---
+++ public/js/simon.js
@@ -0,0 +1,47 @@
+"use strict";
+
+$("#start").click(function() {
+	chooseCircle();
+	displaySequence();
+});
+
+var sequence = [];
+
+//generates a random number and calls one of the circles
+function chooseCircle (){
+	var i = Math.floor((Math.random() * 4)+1);
+	sequence.push(i);
+}
+
+function animateCircle (clickedCircle) { //independent
+	$('[data-id="' + clickedCircle + '"]').animate ({
+		opacity: 1.00
+	}, 1000).animate ({
+		opacity: 0.5
+	}, 1000);
+}
+
+function displaySequence (){
+	sequence.forEach(function(element, index){   // need the index to multiply the timeout
+		setTimeout(function () {
+			animateCircle(element);
+		}, 2000*(index));
+	});
+	console.log(sequence);
+}
+var index = 0;
+
+// continues the sequence if the correct circle is clicked
+$('.circle').click(function(e) {
+	var clickedCircle = $(this).data('id');
+	if (clickedCircle == sequence[index]) {
+		index++;
+		if (index == sequence.length) {
+			index = 0;
+			chooseCircle();
+			displaySequence();
+		}
+	} else if (clickedCircle != sequence[index]) {
+		index = 0;
+	};
+});
